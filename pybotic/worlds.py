722756501524a13a89dd2efe2f1afee4faa7d0b6
00arun00 <arunjoseph.ai@gmail.com>
2020-06-12 03:26:46 -0700

    shifted the classes to dataclass

--- pybotic/worlds.py
+++ pybotic/worlds.py
@@ -1,39 +1,86 @@
 from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
 import numpy as np
+from typing import Dict
+
 from pybotic.utils.world_utils import load_3d_world_map
 
 
+@dataclass
 class World(ABC):
     """
         This is an abstract class representing world
         Never to be directly used in any applications
     """
-    @abstractmethod
+    _boundary: np.ndarray = field(default=None)
+    _obstacles: Dict[str, np.ndarray] = field(default=None)
+    _start: np.ndarray = field(default=None)
+    _goal: np.ndarray = field(default=None)
+
+    def __pre_init__(self):
+        print("asgdags")
+
+    def __post_init__(self):
+        """
+            This is used to validate the inputs
+        """
+        self._robot_pose = self._start
+        for (name, field_type) in self.__annotations__.items():
+            try:
+                if not isinstance(self.__dict__[name], field_type):
+                    current_type = type(self.__dict__[name])
+                    raise ValueError(
+                        f"The field `{name}` was assigned by `{current_type}` instead of `{field_type}`")
+            except Exception as e:
+                if e == "Subscripted generics cannot be used with class and instance checks":
+                    print(name, field_type, type(self.__dict__[name]))
+                    continue
+
+        # validate start and goal
+        if np.shape(self._start) not in {(3, 1), (3,)}:
+            raise ValueError(f'wrong shape {np.shape(self._start)}, {self._start}')
+
+        if np.shape(self._goal) not in {(3, 1), (3,)}:
+            raise ValueError(f'wrong shape {np.shape(self._goal)}')
+
+        if np.shape(self._boundary) not in {(6, 1), (6,)}:
+            raise ValueError(f"wrong boundary shape {np.shape(self._boundary)}")
+
+        for obs_idx in self._obstacles:
+            if np.shape(self._obstacles[obs_idx]) not in {(6, 1), (6,)}:
+                raise ValueError(f"wrong obstacle shape {np.shape(self._obstacles[obs_idx])}")
+
     def get_state(self):
         """
             returns the state of the world
 
             Returns:
-                A dictionary containg
+                A dictionary containg.__annotations__
+                boundary: (numpy.ndarray) boundary of the world
+
                 start: (numpy.ndarray) start location
                 goal: (numpy.ndarray) goal location
                 robot_location: (numpy.ndarray) location of the
                     robot
                 Obstacle_state: (numpy.ndarray) state/location of obstacles
-                boundary: (numpy.ndarray) boundary of the world
 
         """
-        pass
+        return dict(self.__iter__())
 
-    @abstractmethod
+    def __call__(self):
+        return self.get_state()
+
     def update_state(self, robot_action):
         """
-            updates the state of the world, given robot action
+            currently just pass through
 
-            - updates robot's location/state
-            - update obstacles
+            Args:
+                robot_action: (iterable 3d) robot pose
         """
-        pass
+        if np.shape(robot_action) not in {(3, 1), (3,)}:
+            raise ValueError('wrong shape')
+
+        self._robot_pose = robot_action
 
     @abstractmethod
     def render(self):
@@ -42,69 +89,16 @@ class World(ABC):
         """
         pass
 
+    def __iter__(self):
+        for name in self.__dict__:
+            yield name[1:], self.__dict__[name]
 
+
+@dataclass
 class Continous3D_Static(World):
     """
         Implementation of a continous 3D world
     """
-
-    def __init__(self, start, goal, obstacles, boundary):
-        """
-            sets up the world
-
-            Args:
-                start: (numpy.ndarray) start robot_location
-                goal: (numpy.ndarray) goal to reach
-                Obstacle: dict{key:(numpy.ndarray)} for each obstacle
-                boundary: (numpy.ndarray) bounds of the world
-
-            Returns:
-                None
-
-            Raise:
-                TypeError
-        """
-        self.validate_inputs(start, goal, obstacles, boundary)
-
-        self._start = start
-        self._goal = goal
-        self._obstacles = obstacles
-        self._boundary = boundary
-
-    @staticmethod
-    def validate_inputs(start, goal, boundary, obstacles):
-        """
-            function that will run all the validation for
-            inputs before it goes to the class
-
-            constraints:
-                - start : iterable 3d
-                - goal : iterable 3d
-                - boundary: iterable 6d (3d_max, 3d_min)
-                - obstacles: dic{'key',{6d(3d_max, 3d_min)}}
-        """
-        # validate start and goal
-        for ele in [start, goal]:
-            if np.shape(start) not in {(3, 1), (3,)}:
-                raise ValueError('wrong shape')
-
-        if np.shape(boundary) not in {(6, 1), (6,)}:
-            raise ValueError("wrong boundary shape")
-
-        # type check
-        for ele in [start, goal, boundary]:
-            if not all([type(e) in [int, float] for e in ele]):
-                raise TypeError
-
-        if type(obstacles) is not dict():
-            raise TypeError('obstacles should be of type dict')
-
-        for obs_idx in obstacles:
-            if np.shape(obstacles[obs_idx]) not in {(6, 1), (6,)}:
-                raise ValueError("wrong obstacle shape")
-
-            if not all([type(e) in [int, float] for e in obstacles[obs_idx]]):
-                raise TypeError('all obstables should be dict(int,float)')
 
     @classmethod
     def create_from_txt_file(cls, f_name):
@@ -121,3 +115,7 @@ class Continous3D_Static(World):
         if start is None:
             start = np.zeros((3, 1))
         return cls(start, goal, obstacles, boundary)
+
+    def render(self):
+        # TODO: render
+        pass
