f702195011ff4754a63f28ba756b8d80ddf2ea88
00arun00 <arunjoseph.ai@gmail.com>
2020-06-11 18:42:08 -0700

    rewrote abstract class def using builtin methods

--- pybotic/worlds.py
+++ pybotic/worlds.py
@@ -1,28 +1,30 @@
 # this file contains implementations for abstract world class
+from abc import ABC, abstractmethod
 
 
-class world:
+class world(ABC):
     """
         This is an abstract class representing world
         Never to be directly used in any applications
     """
-
+    @abstractmethod
     def get_state(self):
         """
             returns the state of the world
 
             Returns:
                 A dictionary containg
-                start: [numpy.ndarray] start location
-                goal: [numpy.ndarray] goal location
-                robot_location: [numpy.ndarray] location of the
+                start: (numpy.ndarray) start location
+                goal: (numpy.ndarray) goal location
+                robot_location: (numpy.ndarray) location of the
                     robot
-                Obstacle_state: [numpy.ndarray] state/location of obstacles
-                boundary: [numpy.ndarray] boundary of the world
+                Obstacle_state: (numpy.ndarray) state/location of obstacles
+                boundary: (numpy.ndarray) boundary of the world
 
         """
-        raise NotImplementedError('This is an abstract method')
+        pass
 
+    @abstractmethod
     def update_state(self, robot_action):
         """
             updates the state of the world, given robot action
@@ -30,10 +32,11 @@ class world:
             - updates robot's location/state
             - update obstacles
         """
-        raise NotImplementedError('This is an abstract method')
+        pass
 
+    @abstractmethod
     def render(self):
         """
             renders the envirnment
         """
-        raise NotImplementedError("This is an abstract method")
+        pass
