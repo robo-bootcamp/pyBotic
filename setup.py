22d02a61745cd517c440d48b8e3753e034798277
00arun00 <arunjoseph.ai@gmail.com>
2020-06-12 18:36:12 -0700

    remove support for 3.5 since dataclasses are not supported

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Framework :: Robot Framework',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
