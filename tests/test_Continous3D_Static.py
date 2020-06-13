e12b72c96d75427f6d1232e1e445c91488ae6119
00arun00 <arunjoseph.ai@gmail.com>
2020-06-12 17:36:27 -0700

    imporve coverage added setUp

--- tests/test_Continous3D_Static.py
+++ tests/test_Continous3D_Static.py
@@ -1,17 +1,81 @@
 from pybotic.worlds import Continous3D_Static
-from pybotic.utils.world_utils import load_3d_world_map
+import unittest
 import numpy as np
 
 
-def test_continous3d_static():
-    # test valid constructor
-    boundary = np.array([1, 2, 3, 4, 5, 6])
-    obstacles = {'1': np.array([1]*6), '2': np.array([2]*6)}
-    start = np.zeros((3, 1))
-    goal = np.ones((3, 1))
+class TestContinous3DStatic(unittest.TestCase):
+    """
+        Tester for Continous3D_Static
+        test covered:
+            - valid
+            - file not found
+    """
 
-    c = Continous3D_Static(boundary, obstacles, start, goal)
+    def setUp(self):
+        self.boundary = np.array([1, 2, 3, 4, 5, 6])
+        self.obstacles = {'1': np.array([1]*6)}
+        self.start = np.zeros((3, 1))
+        self.goal = np.ones((3, 1))
+        self.c = Continous3D_Static(self.boundary, self.obstacles,
+                                    self.start, self.goal)
 
-    assert isinstance(c, Continous3D_Static)
+    def test_valid(self):
+        """
+            this is will check all the valid cases
+            - valid construction
+            - get state
 
-    c()
+        """
+
+        # valid construction
+        self.assertIsInstance(self.c, Continous3D_Static)
+
+        valid_output = {'boundary': np.array([1, 2, 3, 4, 5, 6]),
+                        'obstacles': {'1': np.array([1]*6)},
+                        'start': np.zeros((3, 1)),
+                        'goal': np.ones((3, 1)),
+                        'robot_pose': np.zeros((3, 1))}
+
+        self.rec_check_dict(valid_output, self.c())
+
+    def test_empty(self):
+        with self.assertRaises(ValueError):
+            Continous3D_Static()
+
+    def test_shape(self):
+        # test boundary
+        with self.assertRaises(ValueError):
+            Continous3D_Static(np.array([1]*5), self.obstacles,
+                               self.start, self.goal)
+
+        # test obstacles
+        with self.assertRaises(ValueError):
+            Continous3D_Static(self.boundary, {'1': np.array([1]*4)},
+                               self.start, self.goal)
+
+        # test start
+        with self.assertRaises(ValueError):
+            Continous3D_Static(self.boundary, self.obstacles,
+                               np.zeros((3, 2)), self.goal)
+
+        # test goal
+        with self.assertRaises(ValueError):
+            Continous3D_Static(self.boundary, self.obstacles,
+                               self.start, np.ones((3, 5)))
+
+    def rec_check_dict(self, a, b):
+        # check keys
+        assert a.keys() == b.keys()
+
+        # ensure that values are same
+        for key in a.keys():
+            if isinstance(a[key], dict):
+                self.rec_check_dict(a[key], b[key])
+                continue
+            assert all(np.equal(a[key], b[key]))
+
+    def test_update_state(self):
+        """
+            test features of update state
+        """
+        self.c.update_state(self.goal)
