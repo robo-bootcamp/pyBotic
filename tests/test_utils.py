c3ccf40b657a34d650dd531760b62cb07a316cb5
00arun00 <arunjoseph.ai@gmail.com>
2020-06-15 03:55:50 -0700

    100% coverage for load_map_3d_from_file

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,11 +11,50 @@ class TestLoad3DWorldMap(unittest.TestCase):
             - file not found
     """
 
+    def setUp(self):
+        self.path = "tests/map_files/"
+
     def test_valid_load(self):
-        f_name = 'tests/sample_world.txt'
+        f_name = 'tests/map_files/sample_world.txt'
         boundary, obstacles, _, _ = load_3d_map_from_file(f_name)
         pass
 
     def test_invalid_file(self):
         with self.assertRaises(FileNotFoundError):
             load_3d_map_from_file("invalid")
+
+    def test_not_supported_format(self):
+        with self.assertRaises(NotImplementedError):
+            load_3d_map_from_file(self.path + "invalid1.json")
+
+    def test_invalid_syntax(self):
+        # wrong key "asdfjkjlfskd: 1, 2, 3, 4, 5, 6"
+        with self.assertRaises(SyntaxError):
+            load_3d_map_from_file(self.path + 'syntax_err1.txt')
+        # random txt "ajlkflkfsdajlkf:"
+        with self.assertRaises(SyntaxError):
+            load_3d_map_from_file(self.path + 'syntax_err2.txt')
+
+    def test_value_errors(self):
+        # repeating keyword
+        with self.assertRaises(ValueError):
+            load_3d_map_from_file(self.path + "value_err1.txt")
+
+    def test_repeated_obstacle_warnings(self):
+        # same obstacle repeated
+        with self.assertWarns(Warning):
+            load_3d_map_from_file(self.path + "warn_obs_repeat.txt")
+
+    def test_missing_boundary(self):
+        # missing boundary
+        with self.assertRaises(KeyError):
+            load_3d_map_from_file(self.path + "no_bound.txt")
+
+    def test_shape_errors(self):
+        for file in range(1, 4):
+            with self.assertRaises(ValueError):
+                load_3d_map_from_file(self.path + f'shape_error{file}.txt')
+
+    def test_keyword_missing(self):
+        with self.assertWarns(Warning):
+            load_3d_map_from_file(self.path + "warn1.txt")
